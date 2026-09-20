5ffbce1abdbd301ad6f9b8774a92984a553255a4
Guilherme Silva <guime.silva333@gmail.com>
2026-09-19 23:14:05 -0300

    chore(setup): inicializa projetos base do frontend (react/vite) e backend (spring boot)

---
+++ 3-Semestre/backend-core/src/main/java/com/activeage/core/ActiveAgeCoreApplication.java
@@ -0,0 +1,13 @@
+package com.activeage.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveAgeCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveAgeCoreApplication.class, args);
+	}
+
+}
---
+++ 3-Semestre/backend-core/src/test/java/com/activeage/core/ActiveAgeCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activeage.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveAgeCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
